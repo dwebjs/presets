00f78e775b51431651daa3b1ea203e5fec59f7d3
feenuhks <32998943+feenuhks@users.noreply.github.com>
2019-11-05 09:22:39 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var DWEB_DOMAIN = 'dweb.local'
+var DEFAULT_REVELATION = [
+  'revelation1.dwebs.io',
+  'revelation2.dwebs.io'
+]
+var DEFAULT_BOOTSTRAP = [
+  'bootstrap1.dwebs.io:6881',
+  'bootstrap2.dwebs.io:6881',
+  'bootstrap3.dwebs.io:6881',
+  'bootstrap4.dwebs.io:6881'
+]
+
+var DEFAULT_OPTS = {
+  dns: { server: DEFAULT_REVELATION, domain: DWEB_DOMAIN },
+  dht: { bootstrap: DEFAULT_BOOTSTRAP }
+}
+
+module.exports = function (opts) {
+  return Object.assign ({}, DEFAULT_OPTS, opts) // opts takes priority
+}
